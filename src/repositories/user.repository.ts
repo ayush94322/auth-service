df753d9363e97b6335f49e477d095983479cc2f5
Ayush Yadav <yadavayush94322@gmail.com>
2026-07-27 13:56:42 +0530

    feat: add user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,29 @@
+import {prisma} from "../lib/prisma.js";
+
+export class UserRepository {
+    async findByEmail(email: string) {
+        return prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+    }
+
+    async findById(id: string) {
+        return prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
+    }
+
+    async create(data: {
+        name: string;
+        email: string;
+        passworHash: string
+    }) {
+        return prisma.user.create({
+            data
+        });
+    }
+}
