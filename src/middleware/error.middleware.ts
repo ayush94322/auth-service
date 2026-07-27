617d0f31257ebc84a3e9be325e6382fa597ec7b3
Ayush Yadav <yadavayush94322@gmail.com>
2026-07-27 18:04:01 +0530

    feat: add errorHandler in error.middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,16 @@
+import { ErrorRequestHandler } from "express";
+import { AppError } from "../errors/AppError.js";
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        });
+    }
+    console.error(err)
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
+};
